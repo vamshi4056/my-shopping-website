b73041e9a24a54d096198d6c3b4af4722b34570e
vamshi4056 <131132203+vamshi4056@users.noreply.github.com>
2023-08-07 13:21:47 +0530

    Add files via upload

---
+++ sta.js
@@ -0,0 +1,16 @@
+let vam = document.querySelector('.sico');
+document.querySelector('#search').onclick = () => {
+    vam.classList.toggle('active');
+}
+let hlo = document.querySelector('.shoping');
+document.querySelector('#cart').onclick = () => {
+    hlo.classList.toggle('active');
+}
+let hloo = document.querySelector('.login');
+document.querySelector('#user').onclick = () => {
+    hloo.classList.toggle('active');
+}
+let hlooo = document.querySelector('.products');
+document.querySelector('.features').onclick = () => {
+    hlooo.classList.toggle('active');
+}
